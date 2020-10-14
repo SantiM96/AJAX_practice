a12d8e37f142418f7a733a4f7baad25f96d4e06f
SantiM96 <smt093@gmail.com>
2020-10-14 17:32:30 -0300

    Finish inicial prectices with this examples

--- js/05.js
+++ js/05.js
@@ -2,28 +2,29 @@
 
 var btnCargar = document.getElementById('cargar');
 
+
 function cargarContenido() {
-  // crearlo
-  var xhr = new XMLHttpRequest();
+    // crearlo
+    var xhr = new XMLHttpRequest();
 
-  // abrirlo
-   xhr.open("GET", "texto_prueba.txt", true);
-   
-   // revisar que cambie
-   xhr.onreadystatechange = function() {
-      //console.log(xhr.readyState);
-      
-      if(xhr.readyState == 4 && xhr.status == 200) {
-          //console.log("Se cargo correctamente");
-          
-          var contenido = document.getElementById('contenido');
-          contenido.innerHTML = xhr.responseText;
-      } 
-   };
+    // abrirlo
+    xhr.open("GET", "texto_prueba.txt", true);
+    
+    // revisar que cambie
+    xhr.onreadystatechange = function() {
+        console.log(xhr.readyState);
+        
+        if(xhr.readyState == 4 && xhr.status == 200) {
+            console.log("Se cargo correctamente");
+            
+            var contenido = document.getElementById('change');
+            contenido.innerHTML = xhr.responseText;
+        } 
+    };
 
-   xhr.send(); 
+    xhr.send(); 
 }
 
-btnCargar.addEventListener('click', cargarContenido );
+btnCargar.addEventListener('click', cargarContenido);
 
 
---
+++ servidor.php
@@ -0,0 +1,10 @@
+<?php 
+    $texnologies = array(
+        'frontend' => 'HTML5, CSS3, JavaScript',
+        'backend' => 'php, MySQL, apache',
+        'fullstack' => 'HTML5, CSS3, JavaScript, php, MySQL, apache'
+    );
+
+    echo json_encode($texnologies);
+
+?>
